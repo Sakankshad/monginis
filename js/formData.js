28294ca0ef78dd1049481df3e5ff6e368838d3c4
Sakankshad <53963736+Sakankshad@users.noreply.github.com>
2022-03-01 15:09:09 +0530

    Response Changed after submit

--- js/formData.js
+++ js/formData.js
@@ -32,7 +32,7 @@ $( document ).ready(function( $ ) {
 			// log a message to the console
 			$('#result').html('<a href="https://docs.google.com/spreadsheets/d/1wB58j0ahcBC-LW4mlDx20aCMhPS6zyMPGJWzBTVEmF4/edit?usp=sharing" target="_blank">Success - see Google Sheet</a>');
 			console.log("Hooray, it worked!");
-			alert("Registration is Closed!");
+			alert("Submitted!");
 		});
 	
 		// callback handler that will be called on failure
